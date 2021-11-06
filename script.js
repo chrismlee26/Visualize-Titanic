390417411d5429b0221184270b07de93ff2e62e4
CHRIS_LEE <chrismlee87@gmail.com>
2021-11-06 12:27:17 -0700

    Add sort functions to script

--- script.js
+++ script.js
@@ -1,23 +1,52 @@
 import data from './titanic-data.js'
 
 // Get a reference to the #titanic
-const titanic = document.querySelector('#titanic')
+const titanic = document.querySelector('#titanic');
 
 // Get a reference to the #titanic
-const displayData = document.querySelector('#data')
+const displayData = document.querySelector('#data');
 
 // Set some styles on the titanic
 // display flex, justifyContent center, alignItems flex-end
-titanic.style.display = 'grid'
-titanic.style.gridTemplateColumns = 'repeat(34, 30px)'
-titanic.style.gridGap = '1px'
+titanic.style.display = 'grid';
+titanic.style.gridTemplateColumns = 'repeat(30, 30px)';
+titanic.style.gridGap = '1px';
 
 // Map over the data and make a new element for each passenger
 const passengers = data.map(p => {
   const el = document.createElement('div')
   titanic.appendChild(el)
   return el
-})
+});
+
+// Sort by Embarkation
+// data.sort((a, b) => {
+//   if (a.fields.embarked < b.fields.embarked) {
+//     return -1
+//   } else if (a.fields.embarked > b.fields.embarked) {
+//     return 1
+//   }
+//   return 0
+// });
+
+// Sort by Gender
+// data.sort((a, b) => {
+//   if (a.fields.sex === 'female') {
+//     return 1
+//   }
+//   return -1
+// });
+
+// Sort by Survival
+// data.sort((a, b) => {
+//   if (a.fields.survived === 'Yes') {
+//     return -1
+//   }
+//   return 1
+// });
+
+
+
 
 // Let's loop over each passenger and set some styles 
 passengers.forEach((p, i) => {
@@ -28,36 +57,4 @@ passengers.forEach((p, i) => {
   p.style.borderRadius = data[i].fields.sex === 'female' ? '50%' : '0'
   const portColor = { S: "tomato", C: "cornflowerblue", Q: "orange", undefined: "green" }
   p.style.backgroundColor = portColor[data[i].fields.embarked]
-})
-
-
-
-
-
-// Challenges - 
-
-// Make the squares a little bigger 15px by 15px. 
-// You'll need to change both the gridTemplateColumn on the
-// titanic and the width and height of each passenger. 
-
-// Change the number of columns on the titanic to 34
-
-
-// Display each passenger as a circle if they are female. 
-// Do this by setting the borderRadius of each passenger. 
-// Match the passenger in passengers to the object data 
-// in the data array by the index. 
-
-
-
-// Display each passengers who did not survive as 
-// opacity 0.5. 
-
-
-
-// Set the backgroundColor of each passenger by their 
-// embarked value. There are three possible values: 
-// 'S', 'C', and 'Q'
-
-
-
+});
